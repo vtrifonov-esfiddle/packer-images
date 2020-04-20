0387d70f8169bb7a28dbd09ec02ee674a65a1201
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-04-21 00:58:50 +0100

    added windows 10 base image

--- winserver-base/Build.ps1
+++ winserver-base/Build.ps1
@@ -5,7 +5,8 @@
 [string] $PACKER_TEMPLATE = "$env:PACKER_TEMPLATE"
 [string] $AnswerIsoDrive = "$env:PACKER_ANSWER_ISO_DRIVE"
 [Nullable[int]] $InstallationImageIndex = $env:PACKER_INSTALLATION_IMAGE_INDEX
-
+[string] $WindowsVersion = "$env:PACKER_WINDOWS_VERSION"
+[string] $Locale = "$env:PACKER_LOCALE"
 function Set-Parameter($Parameter, $Prompt) {
     if ([string]::IsNullOrEmpty($Parameter)) {
         $Parameter = Read-Host -Prompt $Prompt        
@@ -17,6 +18,7 @@ $USERNAME = Set-Parameter $USERNAME  "USERNAME: "
 $PASSWORD = Set-Parameter $PASSWORD "PASSWORD: "
 $VM_OUTPUT_DIRECTORY = Set-Parameter $VM_OUTPUT_DIRECTORY "VM OUTPUT DIRECTORY: "
 $PACKER_TEMPLATE = Set-Parameter $PACKER_TEMPLATE "PACKER TEMPLATE e.g. winserver2016-base.json: "
+$Locale = Set-Parameter $Locale "Locale e.g. en-US: "
 if ([string]::IsNullOrEmpty($AnswerIsoDrive)) {
     $AnswerIsoDrive = "E:"    
 }
@@ -27,7 +29,12 @@ if ($null -eq $InstallationImageIndex) {
 $CurrentPath = $PWD
 cd $PSSCriptRoot
 
-.\GenerateAnswerIso.ps1 -Username $USERNAME -Password $PASSWORD -AnswerIsoDrive $AnswerIsoDrive -InstallationImageIndex $InstallationImageIndex
+.\GenerateAnswerIso.ps1 -Username $USERNAME `
+    -Password $PASSWORD `
+    -AnswerIsoDrive $AnswerIsoDrive `
+    -InstallationImageIndex $InstallationImageIndex `
+    -WindowsVersion $WindowsVersion `
+    -Locale $Locale
 packer build `
      -var "username=$USERNAME" `
      -var "password=$PASSWORD" `
--- winserver-base/GenerateAnswerIso.ps1
+++ winserver-base/GenerateAnswerIso.ps1
@@ -3,9 +3,11 @@ Param(
 	[string] $Username,
 	[Parameter(Mandatory=$true)]
 	[string] $Password,
+	[Parameter(Mandatory=$true)]
+	[string] $WindowsVersion, # Win10 Win2016 
 	[string] $AnswerIsoDrive = "E:",
 	[string] $Locale = "en-GB",
-	# 1 ->  Windows Server 2016 Standard Evaluation
+	# 1 ->  Windows Server 2016 Standard Evaluation/Windows 10 Enterprise Evaluation
 	# 2 ->  Windows Server 2016 Standard Evaluation (Desktop Experience)
 	# 3 ->  Windows Server 2016 Datacenter Evaluation
 	# 4 ->  Windows Server 2016 Datacenter Evaluation (Desktop Experience)
@@ -21,9 +23,14 @@ if (test-path $OutputPath){
 
 function GenerateAutountattend() {
 	$AutounattendXml = New-Object XML
-	$AutounattendXml.Load("$RootPath\Autounattend.template.xml")
+	$AutounattendXml.Load("$RootPath\Autounattend\$WindowsVersion.template.xml")
 		
 	$windowsPE = $AutounattendXml.unattend.settings[0]
+
+	$MicrosoftWindowsInternationalCoreWinPE = $windowsPE.component[0]
+	$MicrosoftWindowsInternationalCoreWinPE.InputLocale = $Locale
+	$MicrosoftWindowsInternationalCoreWinPE.SystemLocale = $Locale
+	$MicrosoftWindowsInternationalCoreWinPE.UserLocale = $Locale
 
 	$InstallFromMetadata = $windowsPE.component.ImageInstall.OSImage.InstallFrom.Metadata
 	$InstallFromMetadata.Value = "$InstallationImageIndex"
@@ -31,16 +38,33 @@ function GenerateAutountattend() {
 	$UserData = $windowsPE.component.UserData[2]
 	$UserData.FullName = $Username
 	$UserData.Organization = $Username
-	
+		
 	$oobeSystem = $AutounattendXml.unattend.settings[2]
-	$UserAccounts = $oobeSystem.component.UserAccounts
+
+	if ($WindowsVersion -eq "Win10") {
+		$MicrosoftWindowsInternationalCore = $oobeSystem.component[0]
+		
+		$MicrosoftWindowsInternationalCore.InputLocale = $Locale
+		$MicrosoftWindowsInternationalCore.SystemLocale = $Locale
+		$MicrosoftWindowsInternationalCore.UILanguage = $Locale
+		$MicrosoftWindowsInternationalCore.UserLocale = $Locale
+	}
+
+	if ($WindowsVersion -eq "Win10") {
+		$AutoLogon = $oobeSystem.component[1].AutoLogon
+		$UserAccounts = $oobeSystem.component[1].UserAccounts
+	}
+	else {
+		$AutoLogon = $oobeSystem.component.AutoLogon
+		$UserAccounts = $oobeSystem.component.UserAccounts
+	}
+	
 	$UserAccounts.AdministratorPassword.Value = $Password
 	$UserAccounts.LocalAccounts.LocalAccount.DisplayName = $Username
 	$UserAccounts.LocalAccounts.LocalAccount.Name = $Username
 	$UserAccounts.LocalAccounts.LocalAccount.Description = $Username
 	$UserAccounts.LocalAccounts.LocalAccount.Password.Value = $Password
 
-	$AutoLogon = $oobeSystem.component.AutoLogon
 	$AutoLogon.Password.Value = $Password
 	$AutoLogon.Username = $Username
 	
@@ -65,12 +89,17 @@ function GenerateAutountattend() {
 	$InstallWindowsUpdates = Get-SynchronousCommand "Install Windows Updates"
 	$InstallWindowsUpdates.CommandLine = Get-PowershellCommandFromAnswerIso "InstallWindowsUpdates.ps1"
 
+	if ($WindowsVersion -eq "Win10") {
+		$SetAutoLoginPassword = Get-SynchronousCommand "Set AutoLoginPassword"
+		$SetAutoLoginPassword.CommandLine = "cmd /c > reg ADD ""HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"" /v DefaultPassword /t REG_SZ /d $Password /f"
+	}
+	
 	$AutounattendXml.Save("$InputPath\Autounattend.xml")
 }
 
 function GenerateSysprepUnattend() {
 	$sysprepXml = New-Object XML
-	$sysprepXml.Load("$RootPath\SysprepUnattend.template.xml")
+	$sysprepXml.Load("$RootPath\SysprepUnattend\$WindowsVersion.template.xml")
 	
 	$oobeSystem = $sysprepXml.unattend.settings[1]
 	$MicrosoftWindowsInternationalCore = $oobeSystem.component[0]
