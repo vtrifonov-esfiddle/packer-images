ca3589e4c43f0ddb6bb275eaf4d03edce240951e
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-04-22 23:28:44 +0100

    - renamed win10-base - shortened winserver vm names

--- windows-base/ExampleBuilds/BuildWin10.ps1
+++ windows-base/ExampleBuilds/BuildWin10.ps1
@@ -1,7 +1,7 @@
 $env:PACKER_USERNAME = "username"
 $env:PACKER_PASSWORD = "password"
 $env:VM_OUTPUT_DIRECTORY = "VMsOutput"
-$env:PACKER_TEMPLATE = "windows10-base.json"
+$env:PACKER_TEMPLATE = "win10-base.json"
 $env:PACKER_WINDOWS_VERSION = "Win10"
 $env:PACKER_LOCALE = "en-GB"
 
