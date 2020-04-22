771e085cc31b71e686d3bb77a98a8711b2260dd9
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-04-22 22:51:34 +0100

    added windows updates scripts

--- windows-base/AnswerIso/Input/EnableMicrosoftUpdates.ps1
+++ windows-base/AnswerIso/Input/EnableMicrosoftUpdates.ps1
@@ -1,2 +1,9 @@
 
-# TODO
+stop-service wuauserv
+&reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v EnableFeaturedSoftware /t REG_DWORD /d 1 /f
+&reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v IncludeRecommendedUpdates /t REG_DWORD /d 1 /f
+
+$MicrosoftUpdate = New-Object -ComObject Microsoft.Update.ServiceManager -Strict 
+$MicrosoftUpdate.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
+
+start-service wuauserv
