9c70ddcfe192f581fcc5a31899f0fa4851c2d325
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-04-28 16:17:06 +0100

    - added github-agent image - fixed docker-compose folder permissions

---
+++ ubuntu/github-agent/Build.ps1
@@ -0,0 +1,24 @@
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $GITHUB_TOKEN,
+    [string] $SSH_USERNAME = "username",
+    [string] $SSH_PASSWORD = "password",
+    [string] $VM_OUTPUT_DIRECTORY = "../VMsOutput",
+    [string] $VM_NAME = "ubuntu-agent-1"
+)
+
+$PACKER_TEMPLATE_NAME = "github-agent"
+
+$CurrentPath = $PWD
+cd "$PSSCriptRoot"
+Write-Host "Running Packer template: $PSSCriptRoot\$PACKER_TEMPLATE_NAME.json"
+packer build `
+     -var "ssh_username=$SSH_USERNAME" `
+     -var "ssh_password=$SSH_PASSWORD" `
+     -var "vm_output_directory=$VM_OUTPUT_DIRECTORY" `
+     -var "base_image_directory=$VM_OUTPUT_DIRECTORY" `
+     -var "GITHUB_TOKEN=$GITHUB_TOKEN" `
+     -var "vm_name=$VM_NAME" `
+     "$PACKER_TEMPLATE_NAME.json"
+cd $CurrentPath
--- ubuntu/ubuntu-docker/scripts/docker-compose.sh
+++ ubuntu/ubuntu-docker/scripts/docker-compose.sh
@@ -1,4 +1,5 @@
 echo "==> Installing docker-compose"
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chown $SSH_USERNAME: /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
