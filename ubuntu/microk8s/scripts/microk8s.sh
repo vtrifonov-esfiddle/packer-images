905689da2eb9a4539203a3d3e2d15bacaf26f4d7
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-03 23:00:21 +0100

    refactoring

--- ubuntu/microk8s/scripts/microk8s.sh
+++ ubuntu/microk8s/scripts/microk8s.sh
@@ -2,9 +2,10 @@
 echo "==> Installing microk8s"
 
 sudo snap install microk8s --classic --channel=1.18/stable
-sudo microk8s status --wait-ready
-sudo microk8s enable dns dashboard registry
-sudo snap alias microk8s.kubectl kubectl
 
 sudo usermod -a -G microk8s $SSH_USERNAME
 sudo chown -f -R $SSH_USERNAME ~/.kube
+
+sudo microk8s status --wait-ready
+sudo microk8s enable dns dashboard registry
+sudo snap alias microk8s.kubectl kubectl
