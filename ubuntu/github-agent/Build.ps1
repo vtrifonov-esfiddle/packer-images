cc9fd0cc5b21e56790255429e05ffd299c147ad5
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-05 23:54:59 +0100

    added base_image_name var to github-agent

--- ubuntu/github-agent/Build.ps1
+++ ubuntu/github-agent/Build.ps1
@@ -5,7 +5,8 @@ Param(
     [string] $SSH_USERNAME = "username",
     [string] $SSH_PASSWORD = "password",
     [string] $VM_OUTPUT_DIRECTORY = "../VMsOutput",
-    [string] $VM_NAME = "ubuntu-agent-1"
+    [string] $VM_NAME = "ubuntu-agent-1",
+    [string] $BASE_IMAGE_NAME = "ubuntu1804-docker"
 )
 
 $PACKER_TEMPLATE_NAME = "github-agent"
@@ -20,5 +21,6 @@ packer build `
      -var "base_image_directory=$VM_OUTPUT_DIRECTORY" `
      -var "GITHUB_TOKEN=$GITHUB_TOKEN" `
      -var "vm_name=$VM_NAME" `
+     -var "base_image_name=$BASE_IMAGE_NAME" `
      "$PACKER_TEMPLATE_NAME.json"
 cd $CurrentPath
