06e5e611a6002b63765a8b639813d5b8dec40b71
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-06 22:20:49 +0100

    make sure base image is explicit ubuntu1804-base

--- ubuntu/ExampleBuilds/BuildUbuntuBase.ps1
+++ ubuntu/ExampleBuilds/BuildUbuntuBase.ps1
@@ -2,6 +2,6 @@ $env:PACKER_USERNAME = "username"
 $env:PACKER_PASSWORD = "password"
 $env:VM_OUTPUT_DIRECTORY = "../VMsOutput"
 $env:PACKER_TEMPLATE_DIRECTORY = "ubuntu-base"
-$env:PACKER_TEMPLATE_NAME = "ubuntu-base"
+$env:PACKER_TEMPLATE_NAME = "ubuntu1804-base"
 
 & ..\Build.ps1
