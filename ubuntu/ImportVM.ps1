b687b1c4e4a85fd5d9fea3f93281cb305bb4f321
Vladimir Trifonov <vladotrifonov@gmail.com>
2020-05-25 13:40:10 +0100

    - improve ImportVM with custom VM name

--- ubuntu/ImportVM.ps1
+++ ubuntu/ImportVM.ps1
@@ -1,11 +1,28 @@
 Param(
     [Parameter(Mandatory=$true)]
-    [string] $VmName,
-    [string] $VmOutputPath = "VMsOutput"
+    [string] $TemplateName,
+    [string] $VmName = $TemplateName,
+    [string] $TemplatesOutputPath = "ImagesOutput"
 )
 
-$VmBasePath = "$VmOutputPath\$VmName"
-$VmFullPath = Get-Childitem -Path $VmBasePath -Recurse -Include *.vmcx
-Write-Host "VM Path: $VmFullPath"
-Import-VM -Path $VmFullPath -Copy -GenerateNewId
+function Import() {
+    $TemplateBasePath = "$TemplatesOutputPath\$TemplateName"
+    $TemplateFullPath = Get-Childitem -Path $TemplateBasePath -Recurse -Include *.vmcx
+    Write-Host "Template Path: $TemplateFullPath"
+    Import-VM -Path $TemplateFullPath -Copy -GenerateNewId
+}
+
+function Rename() {
+    Rename-VM $TemplateName -NewName $VmName
+    $Drive = Get-VMHardDiskDrive -VMName $VmName
+    $ParentDriveDir = Split-Path -Path $Drive.Path
+    $RenamedVmPath = "$ParentDriveDir\$VmName.vhdx"
+    Move-Item $Drive.Path $RenamedVmPath
+    Get-VMHardDiskDrive -VMName $VmName -ControllerType SCSI -ControllerNumber 0 -ControllerLocation 0 | Set-VMHardDiskDrive -Path $RenamedVmPath
+}
+
+Import
+if ($VmName -ne $TemplateName) {
+  Rename
+}
 Start-VM -Name $VmName
